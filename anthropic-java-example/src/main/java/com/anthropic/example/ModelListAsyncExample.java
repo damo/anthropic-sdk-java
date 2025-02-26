dcd805c189f00ba270d898ca3d3a007a586d06dc
Ykoh <ykoh@student.42seoul.kr>
2025-02-26 11:07:55 +0900

    fix(api): support `.list()` without arguments. (#131)
    # Conflicts:
    #	anthropic-java-core/src/main/kotlin/com/anthropic/models/ModelListParams.kt

--- anthropic-java-example/src/main/java/com/anthropic/example/ModelListAsyncExample.java
+++ anthropic-java-example/src/main/java/com/anthropic/example/ModelListAsyncExample.java
@@ -3,7 +3,6 @@ package com.anthropic.example;
 import com.anthropic.client.AnthropicClientAsync;
 import com.anthropic.client.okhttp.AnthropicOkHttpClientAsync;
 import com.anthropic.models.ModelListPageAsync;
-import com.anthropic.models.ModelListParams;
 import java.util.concurrent.CompletableFuture;
 
 public final class ModelListAsyncExample {
@@ -13,9 +12,7 @@ public final class ModelListAsyncExample {
         // Configures using the `ANTHROPIC_API_KEY` environment variable
         AnthropicClientAsync client = AnthropicOkHttpClientAsync.fromEnv();
 
-        CompletableFuture<ModelListPageAsync> pageFuture =
-                // TODO: Update this example once we support `.list()` without arguments.
-                client.models().list(ModelListParams.builder().build());
+        CompletableFuture<ModelListPageAsync> pageFuture = client.models().list();
         pageFuture
                 .thenComposeAsync(page -> page.autoPager()
                         .forEach(
