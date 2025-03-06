8d466355b14512d7a5bddd7d1ed81e98a96af389
Tomer Aberbach <tomer@stainless.com>
2025-03-06 16:54:15 -0500

    chore(ci): run more examples

---
+++ anthropic-java-example/src/main/java/com/anthropic/example/Main.java
@@ -0,0 +1,10 @@
+package com.anthropic.example;
+
+public final class Main {
+    private Main() {}
+
+    public static void main(String[] args) {
+        MessagesExample.main(args);
+        MessagesStreamingExample.main(args);
+    }
+}
