c537d7a73538532a92380ab7d62fe9865f2dc9ab
D Gardner <damien@gardnerhrm.com>
2025-03-17 13:01:19 +0000

    message-accumulator: first draft for review

---
+++ anthropic-java-core/src/main/kotlin/com/anthropic/helpers/MessageAccumulator.kt
@@ -0,0 +1,376 @@
+package com.anthropic.helpers
+
+import com.anthropic.core.JsonObject
+import com.anthropic.core.jsonMapper
+import com.anthropic.models.CitationCharLocation
+import com.anthropic.models.CitationContentBlockLocation
+import com.anthropic.models.CitationPageLocation
+import com.anthropic.models.CitationsDelta
+import com.anthropic.models.ContentBlock
+import com.anthropic.models.InputJsonDelta
+import com.anthropic.models.Message
+import com.anthropic.models.MessageDeltaUsage
+import com.anthropic.models.RawContentBlockDeltaEvent
+import com.anthropic.models.RawContentBlockStartEvent
+import com.anthropic.models.RawContentBlockStopEvent
+import com.anthropic.models.RawMessageDeltaEvent
+import com.anthropic.models.RawMessageStartEvent
+import com.anthropic.models.RawMessageStopEvent
+import com.anthropic.models.RawMessageStreamEvent
+import com.anthropic.models.RedactedThinkingBlock
+import com.anthropic.models.SignatureDelta
+import com.anthropic.models.TextBlock
+import com.anthropic.models.TextCitation
+import com.anthropic.models.TextDelta
+import com.anthropic.models.ThinkingBlock
+import com.anthropic.models.ThinkingDelta
+import com.anthropic.models.ToolUseBlock
+import com.anthropic.models.Usage
+
+/**
+ * An accumulator that constructs a [Message] from a sequence of streamed events. Pass all events
+ * from the `message_start` event to the `message_stop` event to [accumulate] and then call
+ * [message] to get the final accumulated message. The final [Message] will be similar to what would
+ * have been received had the non-streaming API been used.
+ *
+ * A [MessageAccumulator] may only be used to accumulate one message. To accumulate another message,
+ * create another instance of [MessageAccumulator].
+ */
+class MessageAccumulator {
+
+    // TODO: Confirm that the ubiquitous "_additionalProperties()" of the stream events can
+    //   generally be ignored and that it does not provide partial properties that need to be
+    //   accumulated.
+
+    /**
+     * The final accumulated message. Created from the [messageBuilder] when the `message_stop`
+     * event is notified.
+     */
+    private var message: Message? = null
+
+    /**
+     * The message builder used to accumulate the message details. Created when the `message_start`
+     * event is notified.
+     */
+    private var messageBuilder: Message.Builder? = null
+
+    /**
+     * The message usage that accumulates the details of input and output tokens used when creating
+     * the message. Created when the `message_start` event is notified.
+     */
+    // TODO: A "Message.Builder.appendUsage" function would be nice to have.
+    private var messageUsage: Usage? = null
+
+    /**
+     * The indexed collection of content blocks accumulated so far. As `content_block_delta` events
+     * are received, immutable elements here may be replaced with updated instances that hold the
+     * latest accumulation. The keys correspond to the `index` identified in each of the
+     * `content_block_delta` events.
+     */
+    private val messageContent: MutableMap<Long, ContentBlock> = mutableMapOf<Long, ContentBlock>()
+
+    /**
+     * Accumulations of partial JSON strings from `tool_use` content block deltas to form complete
+     * strings on the `content_block_stop` events, as valid JSON strings are required before each
+     * final `tool_use` content block can be created. Only on the `content_block_stop` events are
+     * such blocks created (based on the blocks from the `content_block_start` events); there are
+     * _no_ incremental updates to the starting content blocks as the `tool_use` delta events are
+     * notified. The keys correspond to the `index` identified in each of the `content_block_delta`
+     * events.
+     */
+    private val messageContentInputJson: MutableMap<Long, String> = mutableMapOf<Long, String>()
+
+    companion object {
+        private val JSON_MAPPER = jsonMapper()
+
+        internal fun mergeMessageUsage(usage: Usage, deltaUsage: MessageDeltaUsage): Usage =
+            usage.toBuilder().outputTokens(usage.outputTokens() + deltaUsage.outputTokens()).build()
+
+        internal fun mergeTextDelta(
+            contentBlock: ContentBlock,
+            textDelta: TextDelta,
+        ): ContentBlock {
+            require(contentBlock.isText()) { "Content block is not a text block." }
+            val oldTextBlock = contentBlock.text().get()
+            val newTextBlock =
+                TextBlock.builder()
+                    .from(oldTextBlock)
+                    .text(oldTextBlock.text() + textDelta.text())
+                    .build()
+
+            return ContentBlock.ofText(newTextBlock)
+        }
+
+        internal fun mergeCitationsDelta(
+            contentBlock: ContentBlock,
+            citationsDelta: CitationsDelta,
+        ): ContentBlock {
+            require(contentBlock.isText()) { "Content block is not a text block." }
+            val oldTextBlock = contentBlock.text().get()
+            val newTextBlock =
+                TextBlock.builder()
+                    .from(oldTextBlock)
+                    .addCitation(citationsDeltaToTextCitation(citationsDelta))
+                    .build()
+
+            return ContentBlock.ofText(newTextBlock)
+        }
+
+        internal fun mergeThinkingDelta(
+            contentBlock: ContentBlock,
+            thinkingDelta: ThinkingDelta,
+        ): ContentBlock {
+            require(contentBlock.isThinking()) { "Content block is not a thinking block." }
+            val oldThinkingBlock = contentBlock.thinking().get()
+            val newThinkingBlock =
+                ThinkingBlock.builder()
+                    .from(oldThinkingBlock)
+                    .thinking(oldThinkingBlock.thinking() + thinkingDelta.thinking())
+                    .build()
+
+            return ContentBlock.ofThinking(newThinkingBlock)
+        }
+
+        internal fun mergeSignatureDelta(
+            contentBlock: ContentBlock,
+            signatureDelta: SignatureDelta,
+        ): ContentBlock {
+            // Anthropic Streaming Messages API: "For thinking content, a special `signature_delta`
+            // event is sent just before the `content_block_stop` event. This signature is used to
+            // verify the integrity of the thinking block."
+            //
+            // Therefore, the "merge" here does not concatenate with the existing value of the
+            // `signature` on the `oldThinkingBlock`; the signature is simply set from the given
+            // `signatureDelta`, as there will be only one such delta for the content block.
+            require(contentBlock.isThinking()) { "Content block is not a thinking block." }
+            val oldThinkingBlock = contentBlock.thinking().get()
+            val newThinkingBlock =
+                ThinkingBlock.builder()
+                    .from(oldThinkingBlock)
+                    .signature(signatureDelta.signature())
+                    .build()
+
+            return ContentBlock.ofThinking(newThinkingBlock)
+        }
+
+        internal fun citationsDeltaToTextCitation(citationsDelta: CitationsDelta): TextCitation =
+            // A `CitationsDelta` only holds _one_ citation.
+            citationsDelta
+                .citation()
+                .accept(
+                    object : CitationsDelta.Citation.Visitor<TextCitation> {
+                        override fun visitCharLocation(
+                            charLocation: CitationCharLocation
+                        ): TextCitation = TextCitation.ofCitationCharLocation(charLocation)
+
+                        override fun visitPageLocation(
+                            pageLocation: CitationPageLocation
+                        ): TextCitation = TextCitation.ofCitationPageLocation(pageLocation)
+
+                        override fun visitContentBlockLocation(
+                            contentBlockLocation: CitationContentBlockLocation
+                        ): TextCitation =
+                            TextCitation.ofCitationContentBlockLocation(contentBlockLocation)
+                    }
+                )
+    }
+
+    /**
+     * Gets the final accumulated message. Until the `message_stop` event has been received, a
+     * message will not be available. Wait until all events have been handled by [accumulate] before
+     * calling this method.
+     *
+     * @throws IllegalStateException If called before the `message_stop` event has been accumulated.
+     */
+    // TODO: Decide to call this "finalMessage" or not. There is no concept of a "currentMessage"
+    //   (one partially accumulated), so a "final" prefix to the name seems redundant.
+    fun message(): Message =
+        message ?: throw IllegalStateException("'message_stop' event not yet received.")
+
+    /**
+     * Accumulates a streamed event and uses it to construct a [Message]. When all events, including
+     * the `message_stop` event, have been accumulated, the message can be retrieved by calling
+     * [message].
+     *
+     * @throws IllegalStateException If [accumulate] is called again after the final `message_stop`
+     *   event has been accumulated. A [MessageAccumulator] can only be used to accumulate a single
+     *   [Message].
+     */
+    fun accumulate(event: RawMessageStreamEvent): RawMessageStreamEvent {
+        check(message == null) { "'message_stop' event already received." }
+
+        event.accept(
+            object : RawMessageStreamEvent.Visitor<Unit> {
+                override fun visitStart(start: RawMessageStartEvent) {
+                    check(messageBuilder == null) { "'message_start' event already received." }
+                    messageBuilder = Message.Builder().from(start.message())
+                    messageUsage = start.message().usage()
+                }
+
+                override fun visitDelta(deltaEvent: RawMessageDeltaEvent) {
+                    val delta = deltaEvent.delta()
+
+                    // The Anthropic API allows that there may be "one or more `message_delta`
+                    // events". Here, the interpretation is that if multiple `message_delta` events
+                    // have a `stop_reason`, only the last encountered non-missing `stop_reason`
+                    // value will survive, which may be an _explicit_ `null` value.
+                    if (delta._stopReason().isNull()) {
+                        requireMessageBuilder().stopReason(null)
+                    } else if (!delta._stopReason().isMissing()) {
+                        requireMessageBuilder()
+                            .stopReason(Message.StopReason.of(delta.stopReason().get().asString()))
+                    }
+
+                    // The same applies to the `stop_sequence` string; only the last value will
+                    // survive; multiple `stop_sequence` string values from multiple events are
+                    // _not_ concatenated.
+                    if (delta._stopSequence().isNull()) {
+                        requireMessageBuilder().stopSequence(null)
+                    } else if (!delta._stopSequence().isMissing()) {
+                        requireMessageBuilder().stopSequence(delta.stopSequence().get())
+                    }
+
+                    messageUsage = mergeMessageUsage(requireMessageUsage(), deltaEvent.usage())
+                }
+
+                override fun visitStop(stop: RawMessageStopEvent) {
+                    message =
+                        requireMessageBuilder()
+                            // The indexed content block map is converted to a list with the blocks
+                            // in the indexed order. If there are gaps in the indexes, then the
+                            // indexes of the final list of content blocks will not correspond to
+                            // the indexes of the map entries. However, gaps are not expected and
+                            // what the event indexes were does not matter for the content blocks in
+                            // the final message; it only matters that the relative order of the
+                            // content blocks is preserved.
+                            .content(messageContent.entries.sortedBy { it.key }.map { it.value })
+                            .usage(requireMessageUsage())
+                            .build()
+                    messageBuilder = null
+                }
+
+                override fun visitContentBlockStart(contentBlockStart: RawContentBlockStartEvent) {
+                    val index = contentBlockStart.index()
+
+                    check(messageContent[index] == null) {
+                        "Content block already started for index $index."
+                    }
+
+                    contentBlockStart
+                        .contentBlock()
+                        .accept(
+                            object : RawContentBlockStartEvent.ContentBlock.Visitor<Unit> {
+                                override fun visitText(text: TextBlock) {
+                                    messageContent.put(index, ContentBlock.ofText(text))
+                                }
+
+                                override fun visitToolUse(toolUse: ToolUseBlock) {
+                                    messageContent.put(index, ContentBlock.ofToolUse(toolUse))
+                                }
+
+                                override fun visitThinking(thinking: ThinkingBlock) {
+                                    messageContent.put(index, ContentBlock.ofThinking(thinking))
+                                }
+
+                                override fun visitRedactedThinking(
+                                    redactedThinking: RedactedThinkingBlock
+                                ) {
+                                    // Anthropic Extended Thinking API specification:
+                                    // "`redacted_thinking` blocks will not have any deltas
+                                    // associated and will be sent as a single event."
+                                    messageContent.put(
+                                        index,
+                                        ContentBlock.ofRedactedThinking(redactedThinking),
+                                    )
+                                }
+                            }
+                        )
+                }
+
+                override fun visitContentBlockDelta(contentBlockDelta: RawContentBlockDeltaEvent) {
+                    val index = contentBlockDelta.index()
+                    val oldContentBlock = messageContent[index]
+
+                    check(oldContentBlock != null) { "Content block not started for index $index." }
+
+                    messageContent[index] =
+                        contentBlockDelta
+                            .delta()
+                            .accept(
+                                object : RawContentBlockDeltaEvent.Delta.Visitor<ContentBlock> {
+                                    override fun visitText(text: TextDelta): ContentBlock =
+                                        mergeTextDelta(oldContentBlock, text)
+
+                                    override fun visitInputJson(
+                                        inputJson: InputJsonDelta
+                                    ): ContentBlock {
+                                        val oldInputJson = messageContentInputJson[index]
+
+                                        messageContentInputJson[index] =
+                                            (oldInputJson ?: "") + inputJson.partialJson()
+
+                                        return oldContentBlock // Unchanged until stop event.
+                                    }
+
+                                    override fun visitCitations(
+                                        citations: CitationsDelta
+                                    ): ContentBlock =
+                                        mergeCitationsDelta(oldContentBlock, citations)
+
+                                    override fun visitThinking(
+                                        thinking: ThinkingDelta
+                                    ): ContentBlock = mergeThinkingDelta(oldContentBlock, thinking)
+
+                                    override fun visitSignature(
+                                        signature: SignatureDelta
+                                    ): ContentBlock =
+                                        mergeSignatureDelta(oldContentBlock, signature)
+                                }
+                            )
+                }
+
+                override fun visitContentBlockStop(contentBlockStop: RawContentBlockStopEvent) {
+                    val index = contentBlockStop.index()
+
+                    // Check only that there was a corresponding `content_block_start` event with
+                    // the same index as this `content_block_stop` event. There are no "subtypes" of
+                    // a `RawContentBlockStopEvent` as there are for the corresponding start and
+                    // delta events. It is not possible to validate that the `type` of this event is
+                    // the expected one for the accumulated content with the same `index`, as the
+                    // type is always just `content_block_stop`.
+                    val oldContentBlock = messageContent[index]
+                    check(oldContentBlock != null) { "Content block not started for index $index." }
+
+                    // The `content_block_stop` event for most content block types can be ignored,
+                    // as it carries no data. Where the `index` corresponds to a `tool_use` content
+                    // block, the partial JSON that was concatenated from each delta can now be used
+                    // to update the final `tool_use` content block.
+                    val inputJson = messageContentInputJson[index]
+
+                    if (oldContentBlock.isToolUse()) {
+                        // Check that there was at least one delta, so a potentially-valid `input`
+                        // JSON string was accumulated.
+                        check(inputJson != null) { "Missing input JSON for index $index." }
+                        messageContent[index] =
+                            ContentBlock.ofToolUse(
+                                oldContentBlock
+                                    .asToolUse()
+                                    .toBuilder()
+                                    // Anthropic Streaming Messages API: "the final `tool_use.input`
+                                    // is always an _object_."
+                                    .input(JSON_MAPPER.readValue(inputJson, JsonObject::class.java))
+                                    .build()
+                            )
+                    }
+                }
+            }
+        )
+        return event
+    }
+
+    private fun requireMessageBuilder(): Message.Builder =
+        messageBuilder ?: throw IllegalStateException("'message_start' event not received.")
+
+    private fun requireMessageUsage(): Usage =
+        messageUsage ?: throw IllegalStateException("'message_start' event not received.")
+}
